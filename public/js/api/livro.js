ae395b195ac78891af36881f9cd5209a89b41432
thomazllr <guithomaz270@gmail.com>
2025-04-20 02:05:23 -0300

    feat: refatora api base para ser usada globalmente

--- public/js/api/livro.js
+++ public/js/api/livro.js
@@ -1,16 +1,23 @@
-const API_BASE = "/bibliotech/api/livro.php";
+import { API_BASE } from "../config.js";
 
-export async function createBook(titulo, autor,genero, preco,editora, descricao) {
-  const response = await fetch(API_BASE, {
-    method: 'POST',
+export async function createBook(
+  titulo,
+  autor,
+  genero,
+  preco,
+  editora,
+  descricao
+) {
+  const response = await fetch(API_BASE + "/livro.php", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ titulo, autor, genero, preco, editora, descricao })
+    body: JSON.stringify({ titulo, autor, genero, preco, editora, descricao }),
   });
 
   if (!response.ok) {
-    throw new Error('Falha no Cadastro do Livro');
+    throw new Error("Falha no Cadastro do Livro");
   }
 
   return response.json();
